f2ceb4f17c8923e8b71bb723726868581484059d
Etmo Reis <etmo.mlic@gmail.com>
2025-11-18 22:36:29 +0000

    atualizaçao do meu repositorio

---
+++ Exercicio_Phyton _1/ex03.py
@@ -0,0 +1,6 @@
+def dobro(numero):
+    return numero * 2
+
+valor = int(input("Digite um número inteiro: "))
+resultado = dobro(valor)
+print("O dobro é:", resultado)
---
+++ Exercicio_Phyton _1/ex05.py
@@ -0,0 +1,7 @@
+import math
+ 
+
+a=float(input("Digite o valor do cateto a: "))
+b=float(input("Digite o valor do cateto   b: "))
+hipotenusa=math.hypot(a,b)
+print("O valor da Hipotenusa é de :", hipotenusa)
